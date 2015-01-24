43f61d283ba31c907d908f25c149c1ab277086f5
Brian Lin <brianlin@Brians-MacBook-Pro-3.local>
2015-01-24 12:43:56 -0800

    Initial Commit

---
+++ OnTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnTime
+//
+//  Created by Brian Lin on 1/24/15.
+//  Copyright (c) 2015 Brian Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OnTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OnTime
+//
+//  Created by Brian Lin on 1/24/15.
+//  Copyright (c) 2015 Brian Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
